8f8a0f3b5ad3c72fdad9d404b1b1a09d1d274686
RoaringCow <ersandemircan57@gmail.com>
2023-11-06 23:26:01 +0300

    Daha bunların işini bilmiyorum

--- src/instruction.rs
+++ src/instruction.rs
@@ -62,32 +62,75 @@ pub enum Instruction {
     SkipKeyPressed(Register),           // EX9E - SKP Vx
     SkipKeyNotPressed(Register),        // EXA1 - SKNP Vx
 
-    LoadDelay(Register),                // FX07 - LD Vx, DT
-    WaitKeyPress(Register),             // FX0A - LD Vx, K
-    SetDelay(Register),                 // FX15 - LD DT, Vx
-    SetSound(Register),                 // FX18 - LD ST, Vx
+    LoadDelayTimer(Register),                // FX07 - LD Vx, DT
+    WaitForKeyPress(Register),             // FX0A - LD Vx, K
+    SetDelayTimer(Register),                 // FX15 - LD DT, Vx
+    SetSoundTimer(Register),                 // FX18 - LD ST, Vx
     AddI(Register),                     // FX1E - ADD I, Vx
-    LoadFont(Register),                 // FX29 - LD F, Vx
+    LoadSprite(Register),                 // FX29 - LD F, Vx
     StoreBCD(Register),                 // FX33 - LD B, Vx
     StoreRegisters(Register),           // FX55 - LD [I], Vx
-    LoadMemory(Register),               // FX65 - LD Vx, [I]
+    LoadRegisters(Register),               // FX65 - LD Vx, [I]
 }
 
 impl Instruction {
 
     pub fn new(opcode: OpCode) {
         match opcode.0 & 0xF000 {
-            0x0000 => match opcode.0.ooon() {
+            0x0000 => match opcode.ooon() {
                 0x0000 => Some(Instruction::ClearDisplay),
-                0x000E => Some(Instruction::Return)
-            }
+                0x000E => Some(Instruction::Return),
+                _ => None,
+            },
             0x1000 => Some(Instruction::Jump(opcode.onnn())),
             0x2000 => Some(Instruction::Call(opcode.onnn())),
-            0x3000 => Some(Instruction::SkipEqual(opcode.oxoo, opcode.oonn)),
-            0x4000 => Some(Instruction::SkipNotEqual(opcode.oxoo, opcode.onnn)),
-            0x5000 => Some(Instruction::SkipEqualXY(opcode.oxoo, opcode.ooyo)),
-            0x6000 => Some(Instruction::Load(opcode.oxoo, opcode.oonn)),
-            0x7000 => Some(Instruction::Add(opcode.oxoo, opcode.oonn)),
+            0x3000 => Some(Instruction::SkipIfEqualsByte(opcode.oxoo(), opcode.oonn())),
+            0x4000 => Some(Instruction::SkipIfNotEqualsByte(
+                opcode.oxoo(),
+                opcode.oonn(),
+            )),
+            0x5000 => Some(Instruction::SkipIfEqual(opcode.oxoo(), opcode.ooyo())),
+            0x6000 => Some(Instruction::LoadByte(opcode.oxoo(), opcode.oonn())),
+            0x7000 => Some(Instruction::AddByte(opcode.oxoo(), opcode.oonn())),
+            0x8000 => match opcode.ooon() {
+                0x0000 => Some(Instruction::Move(opcode.oxoo(), opcode.ooyo())),
+                0x0001 => Some(Instruction::Or(opcode.oxoo(), opcode.ooyo())),
+                0x0002 => Some(Instruction::And(opcode.oxoo(), opcode.ooyo())),
+                0x0003 => Some(Instruction::Xor(opcode.oxoo(), opcode.ooyo())),
+                0x0004 => Some(Instruction::Add(opcode.oxoo(), opcode.ooyo())),
+                0x0005 => Some(Instruction::Sub(opcode.oxoo(), opcode.ooyo())),
+                0x0006 => Some(Instruction::ShiftRight(opcode.oxoo())),
+                0x0007 => Some(Instruction::ReverseSub(opcode.oxoo(), opcode.ooyo())),
+                0x000E => Some(Instruction::ShiftLeft(opcode.oxoo())),
+                _ => None,
+            },
+            0x9000 => Some(Instruction::SkipIfNotEqual(opcode.oxoo(), opcode.ooyo())),
+            0xA000 => Some(Instruction::LoadI(opcode.onnn())),
+            0xB000 => Some(Instruction::JumpPlusZero(opcode.onnn())),
+            0xC000 => Some(Instruction::Random(opcode.oxoo(), opcode.oonn())),
+            0xD000 => Some(Instruction::Draw(
+                opcode.oxoo(),
+                opcode.ooyo(),
+                opcode.ooon(),
+            )),
+            0xE000 => match opcode.oonn() {
+                0x009E => Some(Instruction::SkipIfPressed(opcode.oxoo())),
+                0x00A1 => Some(Instruction::SkipIfNotPressed(opcode.oxoo())),
+                _ => None,
+            },
+            0xF000 => match opcode.oonn() {
+                0x0007 => Some(Instruction::LoadDelayTimer(opcode.oxoo())),
+                0x000A => Some(Instruction::WaitForKeyPress(opcode.oxoo())),
+                0x0015 => Some(Instruction::SetDelayTimer(opcode.oxoo())),
+                0x0018 => Some(Instruction::SetSoundTimer(opcode.oxoo())),
+                0x001E => Some(Instruction::AddI(opcode.oxoo())),
+                0x0029 => Some(Instruction::LoadSprite(opcode.oxoo())),
+                0x0033 => Some(Instruction::StoreBCD(opcode.oxoo())),
+                0x0055 => Some(Instruction::StoreRegisters(opcode.oxoo())),
+                0x0065 => Some(Instruction::LoadRegisters(opcode.oxoo())),
+                _ => None,
+            },
+            _ => None,
         }
     }
 }
